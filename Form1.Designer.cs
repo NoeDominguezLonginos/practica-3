6a9d3e6868828b114af23006869a1105efcb63d3
LENOVO T450 <LENOVO T450@DESKTOP-S6J3G1K>
2023-10-27 08:19:25 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace practica_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Rockwell", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(187, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Primer numero";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Rockwell", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(187, 182);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Segundo numero ";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(192, 255, 255);
+            textBox1.Location = new Point(420, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(129, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(192, 255, 255);
+            textBox2.Location = new Point(420, 182);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(129, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Rockwell", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(283, 284);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Sum =";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 255, 128);
+            button1.Font = new Font("Showcard Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(332, 327);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 41);
+            button1.TabIndex = 5;
+            button1.Text = "SUMAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 128, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Sumar numeros ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button1;
+    }
+}
